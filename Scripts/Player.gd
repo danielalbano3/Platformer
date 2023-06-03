8e3e11fb67efcf38d0859548063478c4713268ae
Daniel Albano III <captdane3@gmail.com>
2023-06-03 20:27:52 +0800

    bug fix on jump and fall animation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,10 +9,12 @@ class_name Player
 @export var speed : float = 300.0
 @export var jump_strength : float = 20.0
 
-func _ready():
+var has_jumped : bool = false
+
+func _ready() -> void:
 	velocity = Vector2.ZERO
 
-func _process(delta):
+func _process(delta) -> void:
 	move(delta)
 
 func move(_del : float) -> void:
@@ -24,6 +26,7 @@ func move(_del : float) -> void:
 	
 	if Input.is_action_just_pressed("jump"):
 		direction.y -= jump_strength
+		has_jumped = true
 	
 	if direction.x > 1.0:
 		direction = direction.normalized()
@@ -31,8 +34,6 @@ func move(_del : float) -> void:
 	desired_velocity = direction * speed
 	steering_vector = desired_velocity - velocity
 	velocity += steering_vector * drag_factor
-	
-	
 	
 	move_and_slide()
 	
@@ -42,10 +43,16 @@ func move(_del : float) -> void:
 		else:
 			player_sprite.flip_h = false
 				
+
 	if is_on_floor():
 		if direction != Vector2.ZERO:
 			player_sprite.play("run")
 		elif direction == Vector2.ZERO:
 			player_sprite.play("idle")
+			
+	if has_jumped:
+		has_jumped = false
+		player_sprite.play("jump")
+		
 	
 	
