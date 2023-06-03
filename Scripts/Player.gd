8fed2d60775a533ec71e9122bb09421abe86461a
Daniel Albano III <captdane3@gmail.com>
2023-06-03 17:01:22 +0800

    added move function to player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,3 +1,10 @@
 extends Actor
 class_name Player
 
+func _process(delta):
+	move(delta)
+
+func move(_del : float) -> void:
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_axis("go_left","go_right")
+
