646c74aa423b74664bb8b2fcfea20961e5ed4f31
Daniel Albano III <captdane3@gmail.com>
2023-06-03 18:55:18 +0800

    added movement controls on player (left/right)

--- Scripts/Actor.gd
+++ Scripts/Actor.gd
@@ -3,3 +3,7 @@ class_name Actor
 
 var health : int
 var speed : float
+
+var desired_velocity : Vector2 = Vector2.ZERO
+var steering_vector : Vector2 = Vector2.ZERO
+var drag_factor : float = 0.1
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,30 @@
 extends Actor
 class_name Player
 
+@onready var animation_list : PackedStringArray = $AnimatedSprite2D.sprite_frames.get_animation_names()
+@onready var anim : AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	speed = 300.0
+	velocity = Vector2.ZERO
+
 func _process(delta):
 	move(delta)
 
 func move(_del : float) -> void:
 	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_axis("go_left","go_right")
-
-
+		
+#	if Input.is_action_just_pressed("jump"):
+	
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+	
+	desired_velocity = direction * speed
+	steering_vector = desired_velocity - velocity
+	velocity += steering_vector * drag_factor
+	
+	move_and_slide()
+	
+	
 		
